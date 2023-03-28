d1ab412bada131169ba87194ad264d43aab88731
Shipon Sarder <shipon0142@gmail.com>
2023-03-28 23:26:12 +0600

    bug fixed

---
+++ resources/views/temp_form.blade.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Shipon Sarder</title>
+
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Blog Template">
+    <meta name="author" content="Xiaoying Riley at 3rd Wave Media">
+    <link rel="shortcut icon" href="favicon.ico">
+
+    <!-- FontAwesome JS-->
+    <script defer src="https://use.fontawesome.com/releases/v5.7.1/js/all.js"
+        integrity="sha384-eVEQC9zshBn0rFj4+TU78eNA19HMNigMviK/PU/FFjLXqa/GKPgX58rvt5Z8PLs7" crossorigin="anonymous">
+    </script>
+
+    <!-- Theme CSS -->
+    <link id="theme-style" rel="stylesheet" href="assets/css/theme-1.css">
+</head>
+
+<body>
+
+    <section id="subsection-login-register">
+
+
+        <section class="pb-4">
+            <div class="bg-white border rounded-5">
+
+                <section class="w-100 p-4 d-flex justify-content-center pb-4">
+                    <div style="width: 26rem;">
+    
+                     
+
+                        <div class="tab-content">
+                            <div class="tab-pane fade show active" id="pills-login" role="tabpanel"
+                                aria-labelledby="tab-login">
+                                <form data-bitwarden-watching="1">
+
+    
+                                    <div class="form-outline mb-4">
+                                        <input type="email" id="loginName" class="form-control">
+                                        <label class="form-label" for="loginName" style="margin-left: 0px;">Email or
+                                            username</label>
+
+
+                                    </div>
+
+                                    <!-- Password input -->
+                                    <div class="form-outline mb-4">
+                                        <input type="password" id="loginPassword" class="form-control">
+                                        <label class="form-label" for="loginPassword"
+                                            style="margin-left: 0px;">Password</label>
+
+                                    </div>
+
+                                    <!-- 2 column grid layout -->
+                                    <div class="row mb-4">
+                                        <div class="col-md-6 d-flex justify-content-center">
+                                            <!-- Checkbox -->
+                                            <div class="form-check mb-3 mb-md-0">
+                                                <input class="form-check-input" type="checkbox" value=""
+                                                    id="loginCheck" checked="">
+                                                <label class="form-check-label" for="loginCheck"> Remember me </label>
+                                            </div>
+                                        </div>
+
+                            
+                                    </div>
+                
+                                    <button type="submit" class="btn btn-primary btn-block mb-4">Sign in</button>
+
+                        
+                                </form>
+                            </div>
+
+                        </div>
+        
+                    </div>
+                </section>
+
+
+
+
+
+
+            </div>
+        </section>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+Route::get('/temp-form', function () {
+    return view('temp_form');
+});
